758728f7c86b3108dc527bd48f22cb7883bae57b
santhoshi123654 <alapatdavid@gmail.com>
2021-10-16 22:22:28 +0530

    ADDed the check for the tie difference

--- 10.dayTen/flipCoinSimulator.sh
+++ 10.dayTen/flipCoinSimulator.sh
@@ -3,8 +3,8 @@
 
 heads=0;
 tails=0;
-
-while [[ heads -lt 21 && tails -lt 21 ]]
+diff=0;
+while [[ $heads -lt 21 && $tails -lt 21 || $diff -lt 2 ]]
 do
 	if [ $((RANDOM % 2)) -eq 0 ];
 	then
@@ -12,6 +12,8 @@ do
 	else
 		((tails++));
 	fi
+	diff=$((heads-tails));
+	diff=${diff#-};
 done
 
 echo " heads = $heads and tails = $tails";
