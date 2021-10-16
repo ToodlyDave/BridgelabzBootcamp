4065acb39a7c8395c72c8c56688984b5df2368ca
ToodlyDave <alapatdavid@gmail.com>
2021-10-16 10:06:41 +0530

    removed a file

--- 10.dayTen/sorting.sh
+++ 10.dayTen/sorting.sh
@@ -9,4 +9,25 @@ dict[2]=$((a * b + c))
 dict[3]=$((c + a / b))
 dict[4]=$((a % b + c))
 
+for ((i = 0; i < 5; i++))
+do
+	arr[$i]=${dict[$i]};
+done
 
+echo ${arr[@]};
+
+for ((i = 0; i < 5; i++))
+do
+	for ((j = 0; j < 5 - $i - 1; j++))
+	do
+		if [ arr[$j] -ge arr[$((j + 1))] ]
+		then
+			temp=arr[$j];
+			arr[$j]=${arr[$((j + 1))]};
+			arr[$((j + 1))]=$temp;
+		fi
+
+	done
+done
+
+echo ${arr[@]};
